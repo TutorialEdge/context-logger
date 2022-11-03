58a5c4cc66b4398b0283c0765b25f03f7d6060ad
Elliot Forbes <elliotforbes@Elliots-MBP-2.localdomain>
2022-11-03 19:40:21 +0000

    Adding more wrappers

--- info.go
+++ info.go
@@ -2,9 +2,16 @@ package ctxlog
 
 import (
 	"context"
+
+	"github.com/sirupsen/logrus"
 )
 
 func (l *CtxLogger) Info(ctx context.Context, msg string) {
 	fields := convertFieldsToLogrusFields(getFields(ctx))
 	l.Log.WithFields(fields).Info(msg)
+}
+
+func (l *CtxLogger) InfoFn(ctx context.Context, logFun logrus.LogFunction) {
+	fields := convertFieldsToLogrusFields(getFields(ctx))
+	l.Log.WithFields(fields).InfoFn(logFun)
 }
--- warn.go
+++ warn.go
@@ -1,8 +1,17 @@
 package ctxlog
 
-import "context"
+import (
+	"context"
+
+	"github.com/sirupsen/logrus"
+)
 
 func (l *CtxLogger) Warn(ctx context.Context, msg string) {
 	fields := convertFieldsToLogrusFields(getFields(ctx))
 	l.Log.WithFields(fields).Warn(msg)
+}
+
+func (l *CtxLogger) Warn(ctx context.Context, logFunc logrus.LogFunction) {
+	fields := convertFieldsToLogrusFields(getFields(ctx))
+	l.Log.WithFields(fields).WarnFn(logFunc)
 }
